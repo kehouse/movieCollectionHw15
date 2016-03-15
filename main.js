7737d182966ac6e63991518f62af98c254f34efd
Kathleen House <kathleenhouse@Macintosh-141.local>
2016-03-15 12:55:16 -0400

    added templates file

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-var templates = {};
-templates.movies = [
-  "<article data-id='<%= movies.id %>'>",
-  "<img src='<%= movies.moviePoster %>'>",
-  "<h3><%= movies.title %></h3>",
-  // "<p><%= movies.genre %></p>",
-  "<p><%= movies.storyline %></p>",
-  "<button class='delete'>delete</button>",
-  "<button class='edit'>edit</button>",
-  "</article>"
-].join('');
-
-templates.edit = [
-  '<input type="text" name="image" value="<%= movies.moviePoster %>" placeholder="Image">',
-  '<input type="text" name="title" value="<%= movies.title %>" placeholder="Title">',
-  '<input type="text" name="storyline" value="<%= movies.storyline %>" placeholder="Storyline">',
-  '<input type="button" name="edit" value="edit" class="editButton" placeholder"edit>',
-].join('');
-
 $(document).ready(function(){
   page.init();
 });
